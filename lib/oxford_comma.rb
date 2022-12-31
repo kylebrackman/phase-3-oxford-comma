f070337aed987fdf0a27a8103da8832006323ccd
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-12-31 11:36:27 -0500

    commit tree 9467af5a566899adeb9ac987553670ee483329c9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+  
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
+  end
